0f176066c9ba062e5ad51670b4a9c533b0025cfb
alumnosidat13 <alumnosidat13@gmail.com>
2025-03-27 22:34:07 -0500

    avanzando service

---
+++ src/main/java/pe/edu/idat/demo_ventas_idat/service/CategoryService.java
@@ -0,0 +1,24 @@
+package pe.edu.idat.demo_ventas_idat.service;
+
+import pe.edu.idat.demo_ventas_idat.model.Category;
+import pe.edu.idat.demo_ventas_idat.repository.CategoryRepository;
+
+import java.util.List;
+
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> obtenerCategorias(){
+        // select * from categories;
+        return categoryRepository.findAll();
+    }
+
+    public void guardarCategoria(Category category){
+        //insert into categories values (.....)
+        //update categories set ..... where ....
+        categoryRepository.save(category);
+    }
+}
