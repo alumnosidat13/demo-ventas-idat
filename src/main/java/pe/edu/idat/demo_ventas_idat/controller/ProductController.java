8bf50ebc48509ff953d55ab86ce1dae00c1a7aea
alumnosidat13 <alumnosidat13@gmail.com>
2025-04-03 21:36:50 -0500

    controller

--- src/main/java/pe/edu/idat/demo_ventas_idat/controller/ProductController.java
+++ src/main/java/pe/edu/idat/demo_ventas_idat/controller/ProductController.java
@@ -1,4 +1,23 @@
 package pe.edu.idat.demo_ventas_idat.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.idat.demo_ventas_idat.service.ProductService;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("products",
+                productService.obtenerProductos());
+        return "product/index";
+    }
 }
--- src/main/java/pe/edu/idat/demo_ventas_idat/controller/SupplierController.java
+++ src/main/java/pe/edu/idat/demo_ventas_idat/controller/SupplierController.java
@@ -1,4 +1,15 @@
 package pe.edu.idat.demo_ventas_idat.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.idat.demo_ventas_idat.service.SupplierService;
+
+@Controller
+@RequestMapping("/supplier")
 public class SupplierController {
+    private final SupplierService supplierService;
+    public SupplierController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
+
 }
